f6e1eb7abdbd5ccf46f6274d6e1a02fc7ef61792
navjotsinh189 <navjotsingh189@gmail.com>
2017-10-02 15:58:57 +0000

    navjot.py

--- navjot.py
+++ navjot.py
@@ -1,3 +1,4 @@
+import date, import time
 fin=open("README.md")
 def file_fn(fin):
  word2=list()
@@ -10,3 +11,24 @@ def file_fn(fin):
   if "date" in str(item):
    print(item)
 file_fn(fin)
+
+fin2=open("Crime.csv")
+def file_fn(fin2):
+ word1=list()
+ list=['crime Type', 'crime ID', 'crime Count']
+ for line in fin2:
+  word=line.strip()
+  word1.append(word.split("!"))
+ for item in word1:
+  if "crime Type" in str(item):
+   print(item)
+  if "crime ID" in str(item):
+   print(item)
+file_fn(fin2)
+
+
+def list_crimes(crimes):
+ incidents=make_requests()
+ for i in incidents:
+  crimes.point(float(incident['y']), float(incident['x']),0,0) 
+ crimes.save()
